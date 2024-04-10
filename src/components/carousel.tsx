d5f26cb8eee394ffcf9984d6dc2024d8acbc1d04
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-04-10 00:18:08 -0700

    new carousel

---
+++ src/components/carousel.tsx
@@ -0,0 +1,39 @@
+import type React from "react"
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import type { ProjectSlide } from "@/lib/contentful"
+import { cn } from "@/lib/utils";
+
+interface ProjectsCarouselProps {
+  projects: ProjectSlide[];
+}
+
+export const ProjectsCarousel: React.FC<ProjectsCarouselProps> = ({ projects }) => {
+  const mobilePhotos = ["Clean Location", "Eva Gyroscope"];
+
+  return (
+    <Carousel className="w-full max-w-lg mx-auto">
+      <CarouselContent>
+        {projects.map((project, index) => (
+          <CarouselItem key={index}>
+            <a href={"/project/" + project.slug}>
+              <img src={project.carousselImage.fields.file.url} alt={project.title} className={cn(mobilePhotos.includes(project.title) ? "w-[122px]" : "w-[300px]", "mx-auto")} />
+              <h3 className="text-2xl font-semibold text-center mt-5">{project.title}</h3>
+              <div className="text-center mt-1 font-light text-lg">
+                <p>{project.shortDescription}</p>
+              </div>
+            </a>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
