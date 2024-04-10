fc697d7559fb5190148917b48eb88933978a1a37
Isaac Fimbres <fimbresisaac23@gmail.com>
2024-04-10 00:19:09 -0700

    new contentful utils file

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,4 +1,4 @@
-import contentful from "contentful";
+import contentful, { type EntryFieldTypes } from "contentful";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
@@ -7,3 +7,38 @@ export const contentfulClient = contentful.createClient({
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
+
+export interface Project {
+  contentTypeId: "project",
+  fields: {
+    slug: EntryFieldTypes.Text
+    title: EntryFieldTypes.Text
+    carousselImage: EntryFieldTypes.AssetLink,
+    images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>,
+    shortDescription: EntryFieldTypes.Text,
+    longDescription: EntryFieldTypes.Text,
+    technologies: EntryFieldTypes.Object,
+    features: EntryFieldTypes.Object,
+    codeUrl: EntryFieldTypes.Text,
+    demoUrl: EntryFieldTypes.Text,
+  }
+}
+
+export interface ProjectSlide {
+  slug: EntryFieldTypes.Text
+  title: string
+  carousselImage: {
+    fields: {
+      file: {
+        url: string;
+      }
+    }
+  },
+  images: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>,
+  shortDescription: string,
+  longDescription: EntryFieldTypes.Text,
+  technologies: EntryFieldTypes.Object,
+  features: EntryFieldTypes.Object,
+  codeUrl: EntryFieldTypes.Text,
+  demoUrl: EntryFieldTypes.Text,
+}
