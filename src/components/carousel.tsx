dfb18cbde02a4982a72e330aa4735b164f01e73a
Isaac <fimbresisaac23@gmail.com>
2025-05-13 22:08:22 -0700

    fixing carousel component

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -15,8 +15,6 @@ interface ProjectsCarouselProps {
 }
 
 export const ProjectsCarousel: React.FC<ProjectsCarouselProps> = ({ projects }) => {
-  const mobilePhotos = ["Clean Location", "Eva Gyroscope"];
-
   return (
     <Carousel
       className="w-full max-w-lg mx-auto"
@@ -28,7 +26,7 @@ export const ProjectsCarousel: React.FC<ProjectsCarouselProps> = ({ projects }) 
         {projects.map((project, index) => (
           <CarouselItem key={index}>
             <a href={"/projects/" + project.slug}>
-              <img src={project.carousselImage} alt={project.title} className={cn(mobilePhotos.includes(project.title) ? "w-[122px]" : "w-[300px]", "mx-auto")} />
+              <img src={project.carousselImage} alt={project.title} className={cn(project.device === "mobile" ? "w-[138px]" : "w-[320px]", "mx-auto")} />
               <h3 className="text-2xl font-semibold text-center mt-5">{project.title}</h3>
               <div className="text-center mt-1 font-light text-lg">
                 <p>{project.shortDescription}</p>
